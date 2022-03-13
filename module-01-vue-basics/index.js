d769783f6e2c08b3fee952bba53597e4279c2af9
Dmitriy Petukhov <pdnsite@gmail.com>
2022-03-13 22:17:38 +0300

    feat: start computed

--- module-01-vue-basics/index.js
+++ module-01-vue-basics/index.js
@@ -1,17 +1,29 @@
 const App = {
   data() {
     return {
-      counter: 0
-    }
+      noteInput: '',
+      notePlaceholder: 'Текст заметки',
+      notes: [],
+    };
   },
   methods: {
-    increment() {
-      this.counter++;
+    addNote() {
+      if (this.noteInput.length > 0) {
+        this.notes.push({ id: this.notes.length + 1, title: this.noteInput });
+        this.noteInput = '';
+      }
     },
-    decrement() {
-      this.counter--;
+    removeNote(event, idx) {
+      this.notes.splice(idx, 1);
+      console.log(event);
+    },
+    bindInput(event) {
+      this.noteInput = event.target.value;
+    },
+    toUpperCase(str) {
+      return str.toUpperCase();
     }
-  }
-}
+  },
+};
 
-Vue.createApp(App).mount('#app')
+Vue.createApp(App).mount('#app');
