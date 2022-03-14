cd02ad83de656387bb81774f6f4fdf71d3bb2429
dmitry.petuhov <dmitry.petuhov@profitclicks.ru>
2022-03-14 14:01:25 +0300

    feat: add modifires

--- module-03-what-else-vue-has/index.js
+++ module-03-what-else-vue-has/index.js
@@ -3,9 +3,14 @@ const App = {
     return {
       title: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
       myHtml: '<h3>My Html</h3>',
-      items: [1, 2, 3, 4, 5, 6]
-    }
-  }
-}
+      items: [1, 2],
+    };
+  },
+  computed: {
+    evenItems() {
+      return this.items.filter((item) => item % 2 === 0);
+    },
+  },
+};
 
 Vue.createApp(App).mount('#app');
