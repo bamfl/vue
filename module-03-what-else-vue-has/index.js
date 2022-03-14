359880da67efa148570df391da79916887b4d4da
dmitry.petuhov <dmitry.petuhov@profitclicks.ru>
2022-03-14 13:37:00 +0300

    feat: add v-for with key

--- module-03-what-else-vue-has/index.js
+++ module-03-what-else-vue-has/index.js
@@ -2,7 +2,8 @@ const App = {
   data() {
     return {
       title: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-      myHtml: '<h3>My Html</h3>'
+      myHtml: '<h3>My Html</h3>',
+      items: [1, 2, 3, 4, 5, 6]
     }
   }
 }
