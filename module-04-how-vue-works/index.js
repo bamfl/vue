ee5341b19d817b2a62b20c8b6d7212bfb0307ef3
dmitry.petuhov <dmitry.petuhov@profitclicks.ru>
2022-03-14 15:05:12 +0300

    feat: custom render function of template

--- module-04-how-vue-works/index.js
+++ module-04-how-vue-works/index.js
@@ -1,15 +1,28 @@
+const h = Vue.h;
+
 const App = {
   data() {
     return {
       text: 'Test'
     };
   },
-  template: `
-    <div class="card">
-      <h1>{{ text }}</h1>
-      <button @click="text = 'Изменено'">Изменить текст</button>
-    </div>
-  `
+  // template: `
+  //   <div class="card">
+  //     <h1>{{ text }}</h1>
+  //     <button @click="text = 'Изменено'">Изменить текст</button>
+  //   </div>
+  // `,
+  methods: {
+    changeTitle() {
+      this.text = 'Изменено';
+    }
+  },
+  render() {
+    return h('div', {class: 'card'}, [
+      h('h1', {}, this.text),
+      h('button', {onClick: this.changeTitle}, 'Изменить')
+    ])
+  }
 };
 
 Vue.createApp(App).mount('#app');
