ab478e4513841d35d9b79a407bfde0c6db7deadc
dmitry.petuhov <dmitry.petuhov@profitclicks.ru>
2022-03-14 15:43:34 +0300

    feat: add option API lifecycle

--- module-04-how-vue-works/index.js
+++ module-04-how-vue-works/index.js
@@ -22,7 +22,37 @@ const App = {
       h('h1', {}, this.text),
       h('button', {onClick: this.changeTitle}, 'Изменить')
     ])
+  },
+  beforeCreate() {
+    console.log('beforeCreate');
+  },
+  created() {
+    console.log('created');
+  },
+  beforeMount() {
+    console.log('beforeMount');
+  },
+  mounted() {
+    console.log('mounted');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate');
+  },
+  updated() {
+    console.log('updated');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount');
+  },
+  unmounted() {
+    console.log('unmounted');
   }
 };
 
-Vue.createApp(App).mount('#app');
+const app = Vue.createApp(App)
+
+app.mount('#app');
+
+setTimeout(() => {
+  app.unmount();
+}, 10000);
