5678d298d95579a62255c9dabb76698c1682aafb
dmitry.petuhov <dmitry.petuhov@profitclicks.ru>
2022-03-17 15:41:18 +0300

    feat: start vue form

---
+++ module-07-vue-forms/src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="container">
+    <form class="card" @submit.prevent="submit">
+      <h1>Vue форма</h1>
+      <div class="form-control">
+        <label for="name">Как тебя зовут?</label>
+        <input 
+          type="text" 
+          id="name" 
+          placeholder="Введи имя"
+          v-model.trim="name"
+        />
+      </div>
+
+      <div class="form-control">
+        <label for="age">Выбери возраст</label>
+        <input 
+          type="number" 
+          id="age" 
+          v-model.number="age"
+        />
+      </div>
+
+      <div class="form-control">
+        <label for="city">Твой город</label>
+        <select id="city">
+          <option value="spb">Санкт-Петербург</option>
+          <option value="msk">Москва</option>
+          <option value="kzn">Казань</option>
+          <option selected value="nsk">Новосибирск</option>
+        </select>
+      </div>
+
+      <div class="form-checkbox">
+        <span class="label">Готов к переезду в Токио?</span>
+        <div class="checkbox">
+          <label><input type="radio" name="trip" /> Да</label>
+        </div>
+
+        <div class="checkbox">
+          <label><input type="radio" name="trip" /> Нет</label>
+        </div>
+      </div>
+
+      <div class="form-checkbox">
+        <span class="label">Что знаешь во Vue?</span>
+        <div class="checkbox">
+          <label><input type="checkbox" /> Vuex</label>
+        </div>
+        <div class="checkbox">
+          <label><input type="checkbox" /> Vue CLI</label>
+        </div>
+        <div class="checkbox">
+          <label><input type="checkbox" /> Vue Router</label>
+        </div>
+      </div>
+
+      <button type="submit" class="btn primary">Отправить</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      age: 20
+    }
+  },
+  methods: {
+    submit() {
+      const formData = {
+        name: this.name,
+        age: this.age
+      };
+
+      console.log(formData);
+    }
+  }
+};
+</script>
+
+<style></style>
