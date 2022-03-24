ebb7ee548c3b33634500d26f622b4568a8b4eada
Dmitriy Petukhov <pdnsite@gmail.com>
2022-03-24 21:11:35 +0300

    feat: add custopm directive

--- module-09-vue-mixins-directives-plugins/src/App.vue
+++ module-09-vue-mixins-directives-plugins/src/App.vue
@@ -1,30 +1,30 @@
 <template>
   <div class="container">
-    <AppAlert 
-      type="primary"
-      closable
-    >
+    <div class="form-control">
+      <input v-focus type="text" />
+    </div>
+
+    <AppAlert type="primary" closable>
       <template v-slot:title>Title</template>
       <template v-slot:text>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, cupiditate.</template>
     </AppAlert>
 
-    <AppAlert1 
-      type="primary"
-      closable
-    >
+    <AppAlert1 type="primary" closable>
       <template v-slot:title>Title</template>
       <template v-slot:text>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, cupiditate.</template>
     </AppAlert1>
   </div>
-
 </template>
 
 <script>
-import AppAlert1 from './components/AppAlert1.vue'
+import AppAlert1 from './components/AppAlert1.vue';
 import AppAlert from './components/AppAlert.vue';
+import VFocus from './directives/VFocus.js';
 
 export default {
   components: { AppAlert, AppAlert1 },
-}
+  directives: {
+    focus: VFocus
+  }
+};
 </script>
-
