76749b9e4610a8162fb82739b45e2d41bba1a43e
dmitry.petuhov <dmitry.petuhov@profitclicks.ru>
2022-03-29 15:33:47 +0300

    feat: add teleport

--- module-09-vue-mixins-directives-plugins/src/App.vue
+++ module-09-vue-mixins-directives-plugins/src/App.vue
@@ -21,18 +21,34 @@
       <template v-slot:title>Title</template>
       <template v-slot:text>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, cupiditate.</template>
     </AppAlert1>
+
+    <button class="btn primary" style="margin-top: 20px" @click="modal = true">Open modal</button>
+    
+    <teleport to="body">
+      <AppModal v-if="modal" @closeModal="modal = false">
+        <template #title>Modal window</template>
+        <template #text>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi accusantium, tenetur quod dolores molestias deserunt corrupti officiis! Velit mollitia, eius ad rerum, voluptates totam reprehenderit beatae, nulla placeat deserunt doloribus.</template>
+      </AppModal>
+    </teleport>
   </div>
+  
 </template>
 
 <script>
+import AppModal from './components/AppModal.vue'
 import AppAlert1 from './components/AppAlert1.vue';
 import AppAlert from './components/AppAlert.vue';
 import VFocus from './directives/VFocus.js';
 
 export default {
-  components: { AppAlert, AppAlert1 },
+  components: { AppAlert, AppAlert1, AppModal },
   directives: {
     focus: VFocus
+  },
+  data() {
+    return {
+      modal: false
+    }
   },
   inject: ['changeLangI18n'],
   methods: {
---
+++ module-09-vue-mixins-directives-plugins/src/components/AppModal.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="modal-backdrop" @click="$emit('closeModal')">
+    <div class="modal">
+      <h2><slot name="title">Modal</slot></h2>
+      <p><slot name="text"></slot></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['closeModal']
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
