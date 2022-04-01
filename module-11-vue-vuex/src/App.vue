06fa51e8b50cb384d848dc0bf24de3183d0708a1
dmitry.petuhov <dmitry.petuhov@profitclicks.ru>
2022-04-01 15:34:30 +0300

    feat: add vuex mappers

--- module-11-vue-vuex/src/App.vue
+++ module-11-vue-vuex/src/App.vue
@@ -5,27 +5,22 @@
       <h1>Про Vuex</h1>
       <h2>Счетчик {{ counter }}</h2>
       <button class="btn primary" @click="increment">Добавить</button>
-      <button class="btn primary" @click="addNumAsync">Добавить асинхронно с задержкой</button>
+      <button class="btn primary" @click="addAsync({ value: 100 })">Добавить асинхронно с задержкой</button>
     </div>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapMutations, mapActions } from 'vuex';
 import TheNavbar from './components/TheNavbar';
 export default {
   components: { TheNavbar },
   computed: {
-    counter() {
-      return this.$store.getters.counter;
-    },
+    ...mapGetters(['counter']),
   },
   methods: {
-    increment() {
-      this.$store.commit('increment');
-    },
-    addNumAsync() {
-      this.$store.dispatch('addAsync', { value: 100 });
-    },
+    ...mapMutations(['increment']),
+    ...mapActions(['addAsync']),
   },
 };
 </script>
--- module-11-vue-vuex/src/components/TheNavbar.vue
+++ module-11-vue-vuex/src/components/TheNavbar.vue
@@ -1,31 +1,29 @@
 <template>
   <header class="navbar">
     <div>Vuex {{ counter }} ({{ doubleCounter }})</div>
-    <button class="btn" @click="addNum">Добавить</button>
+    <button class="btn" @click="addNum({ value: 10 })">Добавить 10</button>
   </header>
 </template>
 
 <script>
+import { mapGetters, mapMutations } from 'vuex';
+
 export default {
   computed: {
-    counter() {
-      return this.$store.getters.counter;
-    },
-    doubleCounter() {
-      return this.$store.getters.doubleCounter;
-    },
+    ...mapGetters(['counter', 'doubleCounter']),
   },
   methods: {
-    addNum() {
-      // this.$store.commit('addNum', 10);
-      // or
-      this.$store.commit('addNum', { value: 10 });
-      // or
-      // this.$store.commit({
-      //   type: 'addNum',
-      //   value: 10,
-      // });
-    },
+    ...mapMutations(['addNum']),
+    // or
+    // ...mapMutations(['addNum']),
+    // addTen() {
+    //   this.addNum({ value: 10 });
+    // }
+    // or
+    // ...mapMutations({ addTen: 'addNum'}),
+    // addNum() {
+    //   this.addTen({ value: 10 });
+    // }
   },
 };
 </script>
