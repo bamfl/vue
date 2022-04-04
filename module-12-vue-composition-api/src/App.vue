d0efe1ad0b268863ebcb9bf21086c4a445032053
dmitry.petuhov <dmitry.petuhov@profitclicks.ru>
2022-04-04 15:55:31 +0300

    feat: start composition api

---
+++ module-12-vue-composition-api/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <h1>Vue Composition Api</h1>
+      <hr>
+      <p>Название: <strong>{{ framework }}</strong></p>
+      <p>Версия: <strong>{{ version }}</strong></p>
+
+      <button class="btn" @click="changeVersion">Изменить</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const framework = ref('VueJS');
+const version = ref(2);
+
+function changeVersion() {
+  framework.value = 'VueJS!';
+  version.value = 3;
+}
+</script>
