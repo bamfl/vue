17fb0a22f53b1dc7fb68ebf2e55144e27f020927
dmitry.petuhov <dmitry.petuhov@profitclicks.ru>
2022-04-05 08:17:36 +0300

    feat: add ref and reactive

--- module-12-vue-composition-api/src/App.vue
+++ module-12-vue-composition-api/src/App.vue
@@ -6,19 +6,51 @@
       <p>Название: <strong>{{ framework }}</strong></p>
       <p>Версия: <strong>{{ version }}</strong></p>
 
-      <button class="btn" @click="changeVersion">Изменить</button>
+      <div class="row">
+        <div class="mr-10">{{ car.year }}</div>
+        <div>{{ car.brand }}</div>
+      </div>
+
+      <div class="row">
+        <div class="mr-10">{{ plane.passangers }}</div>
+        <div>{{ plane.year }}</div>
+      </div>
+
+      <button class="btn" @click="changeInfo">Изменить</button>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
 const framework = ref('VueJS');
 const version = ref(2);
 
-function changeVersion() {
+const car = ref({
+  year: 2020,
+  brand: 'Mazda'
+});
+
+const plane = reactive({
+  passangers: 200,
+  year: 2010
+})
+
+function changeInfo() {
+  console.log(framework); // framework - RefImpl
+  console.log(framework.value); // framework.value - value
   framework.value = 'VueJS!';
   version.value = 3;
+
+  console.log(car); // car - RefImpl
+  console.log(car.value); // car.value - Proxy
+  car.value.year = 2022;
+  car.value.brand = 'Toyota';
+
+  console.log(plane); // plane - Proxy
+  console.log(plane.passangers); // plane.passangers - value
+  plane.passangers = 150;
+  plane.year = 2014;
 }
 </script>
